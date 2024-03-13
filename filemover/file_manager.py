bf927aad558f524723240bf8d87c09614b07651b
holenya123a <nholenya@gmail.com>
2024-03-13 19:53:26 +0100

    aded various methods to manipulate files

--- filemover/file_manager.py
+++ filemover/file_manager.py
@@ -1,14 +1,29 @@
 import shutil
 import os
 
+
 class FileHandler:
-    def __init__(self, file_path:str):
-        self.file_path = file_path
+    def __init__(self, dir_path: str):
+        self.dir_path = dir_path
+
+    def list_dirs(self):
+        return os.listdir(self.dir_path)
 
     def move_file(self, destination_path):
-        shutil.move(self.file_path, destination_path)
+        shutil.move(self.dir_path, destination_path)
 
     def delete_file(self):
-        os.remove(self.file_path)
-    
-    
+        os.remove(self.dir_path)
+
+    def search_duplicates(self, files_list):
+        print(type(files_list))
+
+    def remove_duplicates(self):
+        print('hello')
+
+
+file_handler = FileHandler(os.environ['DIR_PATH'])
+print(file_handler.dir_path)
+dir_path = file_handler.dir_path
+list_of_files = file_handler.list_dirs()
+print(list_of_files)
