38cc8ee74e4fad663d001350cf1c4ce76f288a6a
holenya123a <nholenya@gmail.com>
2024-03-23 21:57:42 +0100

    add get duplicates_files function

--- filemover/__init__.py
+++ filemover/__init__.py
@@ -1,5 +1,6 @@
-import argparse
 from file_manager import FileHandler
+import argparse
+import pprint
 import os
 
 
@@ -12,19 +13,40 @@ parser.add_argument('-p', '--path', type=str,
 
 
 def main(path=None):
-
     if path is None:
         try:
             path = os.environ['DIR_PATH']
         except KeyError:
-            print("The environment variable 'DIR_PATH' is not defined.")
+            pprint.pprint(
+                "The environment variable 'DIR_PATH' is not defined.")
             return
+    files_info = get_duplicates_files(path)
+    pprint.pprint(files_info)
 
-    file_handler = FileHandler(path)
-    files = file_handler.list_files()
+
+def get_duplicates_files(path: str):
+
+    files_info: dict = {}
+    file_handler: FileHandler = FileHandler(path)
+    files: list = file_handler.list_files()
+    keys_to_delete: dict = {}
+
+    for file in files:
+        file_hash = file_handler.create_hash(file)
+
+        if file_hash not in files_info:
+            keys_to_delete[file_hash] = None
+            files_info[file_hash] = [file]
+        else:
+            del keys_to_delete[file_hash]
+            files_info[file_hash].append(file)
+
+    for key in keys_to_delete:
+        del files_info[key]
+
+    return files_info
 
 
 args = parser.parse_args()
 dir_path = args.path
-print(dir_path, '<____ dir path')
 main(dir_path)
